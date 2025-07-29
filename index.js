ee8429563ad077b32ad242aeffa224d3236e749a
Ahmad Mudassir <108348781+AhmadMudassir@users.noreply.github.com>
2025-07-29 20:22:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ app.get('/', (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
